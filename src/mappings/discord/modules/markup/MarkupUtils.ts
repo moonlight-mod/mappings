f4d3f6f603d1dcb8cddbec0c099a2fc53bea8f80
NotNite <hi@notnite.com>
2024-10-07 15:56:08 -0400

    MarkupUtils: fix parse type (closes #5)

--- src/mappings/discord/modules/markup/MarkupUtils.ts
+++ src/mappings/discord/modules/markup/MarkupUtils.ts
@@ -1,8 +1,28 @@
 import register from "../../../../registry";
 import { ReactElement } from "react";
 
+// This isn't everything, but it's most of it
+export type MarkupConfig = {
+  hideSimpleEmbedContent?: boolean;
+  formatInline?: boolean;
+  noStyleAndInteraction?: boolean;
+  isInteracting?: boolean;
+  allowHeading?: boolean;
+  allowList?: boolean;
+  allowSubtext?: boolean;
+  allowDevLinks?: boolean;
+  previewLinkTarget?: boolean;
+  shouldFilterKeywords?: boolean;
+  viewingChannelId?: boolean;
+};
+
 export type MarkupUtils = {
-  parse: (text: string) => ReactElement;
+  parse: (
+    text: string,
+    inline?: boolean,
+    config?: MarkupConfig
+    // also a fourth argument here but I don't know what it is
+  ) => ReactElement;
 };
 
 register((moonmap) => {
