46b5b2a649209c1f9f1486d228870fc7d0f1fafa
Cynthia Foxwell <gamers@riseup.net>
2025-01-29 14:04:10 -0700

    buildscript: fixes for npm stubs being batch files in themselves (caused early exits)

--- build.bat
+++ build.bat
@@ -1,9 +1,9 @@
 node generate.js types --write
 node generate.js imports --write
-prettier --write ./src
+call prettier --write ./src
 
 node generate.js declares "@moonlight-mod/wp/" > ..\moonlight\packages\types\src\mappings.d.ts
-prettier --write ..\moonlight\packages\types\src\mappings.d.ts
+call prettier --write ..\moonlight\packages\types\src\mappings.d.ts
 
-pnpm run clean
-pnpm run build
+call pnpm run clean
+call pnpm run build
